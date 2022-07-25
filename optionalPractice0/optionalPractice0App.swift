ef0062c80fe1abf20ea49e6ae68f61cc7076d1e7
Wongi <hpurs95@gmail.com>
2022-07-25 15:07:10 +0900

    Initial Commit

---
+++ optionalPractice0/optionalPractice0App.swift
@@ -0,0 +1,17 @@
+//
+//  optionalPractice0App.swift
+//  optionalPractice0
+//
+//  Created by MacBook Air on 2022/07/25.
+//
+
+import SwiftUI
+
+@main
+struct optionalPractice0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
